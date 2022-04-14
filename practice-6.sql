c49ee9527be36c4f6661c826609b68b41c6b85d9
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-14 20:04:23 +0000

    6

--- practice-6.sql
+++ practice-6.sql
@@ -1,5 +1,7 @@
 -- How many reviews are written per neighborhood?
-
+SELECT neighborhood, count(reviews.comments)
+FROM listings INNER JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY neighborhood
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
 -- | Archer Heights         | 541      |
